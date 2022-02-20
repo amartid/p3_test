b13bee5ceae5563da4190bb7da4ef964b2776095
whatfreedom <angelos95gr@gmail.com>
2022-02-20 20:11:32 +0200

    FileUtil

--- app/src/main/java/com/poupa/vinylmusicplayer/util/FileUtil.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/util/FileUtil.java
@@ -85,43 +85,50 @@ public final class FileUtil {
             }
         }
     }
+    
+    public static boolean checkFileMimeType(File file, String mimeType,MimeTypeMap mimeTypeMap) {
 
-    public static boolean fileIsMimeType(File file, String mimeType, MimeTypeMap mimeTypeMap) {
-        if (mimeType == null || mimeType.equals("*/*")) {
+        String fileExtension = filename.substring(dotPos + 1).toLowerCase();
+        String fileType = mimeTypeMap.getMimeTypeFromExtension(fileExtension);
+    	
+        if (!checkFilenameMimeTypeMap(file,mimeTypeMap))
+    		return false;
+        // check the 'type/subtype' pattern
+        if (fileType.equals(mimeType)) {
             return true;
-        } else {
-            // get the file mime type
-            String filename = file.toURI().toString();
-            int dotPos = filename.lastIndexOf('.');
-            if (dotPos == -1) {
-                return false;
-            }
-            String fileExtension = filename.substring(dotPos + 1).toLowerCase();
-            String fileType = mimeTypeMap.getMimeTypeFromExtension(fileExtension);
-            if (fileType == null) {
-                return false;
-            }
-            // check the 'type/subtype' pattern
-            if (fileType.equals(mimeType)) {
-                return true;
-            }
-            // check the 'type/*' pattern
-            int mimeTypeDelimiter = mimeType.lastIndexOf('/');
-            if (mimeTypeDelimiter == -1) {
-                return false;
-            }
-            String mimeTypeMainType = mimeType.substring(0, mimeTypeDelimiter);
-            String mimeTypeSubtype = mimeType.substring(mimeTypeDelimiter + 1);
-            if (!mimeTypeSubtype.equals("*")) {
-                return false;
-            }
-            int fileTypeDelimiter = fileType.lastIndexOf('/');
-            if (fileTypeDelimiter == -1) {
-                return false;
-            }
-            String fileTypeMainType = fileType.substring(0, fileTypeDelimiter);
-            return fileTypeMainType.equals(mimeTypeMainType);
         }
+        // check the 'type/*' pattern
+        int mimeTypeDelimiter = mimeType.lastIndexOf('/');
+        if (mimeTypeDelimiter == -1) return false;
+        
+        String mimeTypeMainType = mimeType.substring(0, mimeTypeDelimiter);
+        String mimeTypeSubtype = mimeType.substring(mimeTypeDelimiter + 1);
+        if (!mimeTypeSubtype.equals("*")) return false;
+        
+        int fileTypeDelimiter = fileType.lastIndexOf('/');
+        if (fileTypeDelimiter == -1) return false;
+        
+        String fileTypeMainType = fileType.substring(0, fileTypeDelimiter);
+        return fileTypeMainType.equals(mimeTypeMainType);
+    }
+    
+    public static boolean checkFilenameMimeTypeMap(File file, MimeTypeMap mimeTypeMap) {
+    	// get the file mime type
+        String filename = file.toURI().toString();
+        int dotPos = filename.lastIndexOf('.');
+        if (dotPos == -1) {
+            return false;
+        }
+        String fileExtension = filename.substring(dotPos + 1).toLowerCase();
+        String fileType = mimeTypeMap.getMimeTypeFromExtension(fileExtension);
+        if (fileType == null) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static boolean fileIsMimeType(File file, String mimeType, MimeTypeMap mimeTypeMap) {
+    	return (mimeType == null || mimeType.equals("*/*"))?true:checkFileMimeType(file,mimeType,mimeTypeMap);
     }
 
     public static String stripExtension(String str) {
